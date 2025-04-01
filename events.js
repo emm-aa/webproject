a8624bc8b1ecb09554cf100cf9e4c12bda8ed8a2
maggie256 <irenemargi256@gmail.com>
2025-04-01 14:42:37 +0300

    Added events.js file

--- events.js
+++ events.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", 
     function() {
-        const calendar = document.getElementById("event-calendar");
+        
     const today = new Date();
-
-    calendar.innerHTML = '<p>The date today: ${today.toDateString()}</p>';
-});
+    document.getElementById("event-calendar").innerHTML = `The date today: ${today.toDateString()}`;
+}
+);
 
  
